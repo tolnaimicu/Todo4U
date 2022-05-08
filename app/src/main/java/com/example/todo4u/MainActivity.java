19ebe26027f2ee10299c5773af068fb09fe43b0a
BiancaMilitaru <bianca.militaru@yahoo.com>
2022-05-08 19:57:08 +0200

    Struggling to fix nav components No success :/

--- app/src/main/java/com/example/todo4u/MainActivity.java
+++ app/src/main/java/com/example/todo4u/MainActivity.java
@@ -1,55 +1,37 @@
 package com.example.todo4u;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.ActionCodeSettings;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.EmailAuthProvider;
-import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthSettings;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GithubAuthProvider;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
-import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.auth.PlayGamesAuthProvider;
-import com.google.firebase.auth.SignInMethodQueryResult;
-import com.google.firebase.auth.UserInfo;
-import com.google.firebase.auth.UserProfileChangeRequest;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         checkCurrentUser();
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        /// TODO: 08.05.2022  setSupportActionBar(toolbar);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        /// TODO: 08.05.2022  NavigationUI.setupActionBarWithNavController(this, navController);
     }
 
     public void checkCurrentUser() {
@@ -60,8 +42,6 @@ public class MainActivity extends AppCompatActivity {
             startLoginActivity();
         }
     }
-
-
 
     private void startLoginActivity() {
         startActivity(new Intent(this, LogInIntent.class));
@@ -78,7 +58,15 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /// TODO: 08.05.2022  return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
+        return true;
+    }
 
 
 }
