1b49bda9640d9e2219a4c8925212954b4895da7e
BiancaMilitaru <bianca.militaru@yahoo.com>
2022-05-14 18:44:28 +0200

    MyFriends fully working with dummy data

---
+++ app/src/main/java/com/example/todo4u/Adapters/MemberAdapter.java
@@ -0,0 +1,67 @@
+package com.example.todo4u.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todo4u.Models.Board;
+import com.example.todo4u.Models.Member;
+import com.example.todo4u.R;
+
+import java.util.ArrayList;
+
+public class MemberAdapter extends RecyclerView.Adapter<MemberAdapter.ViewHolder>{
+
+    private OnClickListener listener;
+    private ArrayList<Member> members;
+
+    public MemberAdapter(){
+        members = new ArrayList<>();
+        members.add(new Member("hello","hello"));
+        members.add(new Member("hello","hello"));
+        members.add(new Member("hello","hello"));
+        members.add(new Member("hello","hello"));
+        members.add(new Member("hello","hello"));
+    }
+
+    public void setOnClickListener(MemberAdapter.OnClickListener listener){
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.fragment_member_item, parent, false);
+        return new MemberAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.nickName.setText(members.get(position).getNickName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return members.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView nickName;
+
+        ViewHolder(View itemView){
+            super(itemView);
+            this.nickName = itemView.findViewById(R.id.nick_name);
+            itemView.setOnClickListener(v -> listener.onClick(members.get(getBindingAdapterPosition())));
+        }
+    }
+
+    public interface OnClickListener {
+        void onClick(Member member);
+    }
+}
---
+++ app/src/main/java/com/example/todo4u/Fragments/MembersFragment.java
@@ -0,0 +1,52 @@
+package com.example.todo4u.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.todo4u.Adapters.BoardAdapter;
+import com.example.todo4u.Adapters.MemberAdapter;
+import com.example.todo4u.R;
+
+public class MembersFragment extends Fragment {
+
+    RecyclerView membersList;
+    MemberAdapter memberAdapter;
+    Context context;
+
+    public MembersFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        context = container.getContext();
+        View view = inflater.inflate(R.layout.fragment_members, container, false);
+        membersList = view.findViewById(R.id.recyclerViewMember);
+        membersList.hasFixedSize();
+        membersList.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        memberAdapter = new MemberAdapter();
+        membersList.setAdapter(memberAdapter);
+
+        memberAdapter.setOnClickListener(board -> {
+            Toast.makeText(context, board.getNickName(), Toast.LENGTH_SHORT).show();
+        });
+
+        return view;
+    }
+}
