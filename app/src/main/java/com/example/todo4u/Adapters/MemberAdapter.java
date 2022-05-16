4f770c3c89acbd89e12cb9087fd245753ffddafc
kyratolnai11 <266375@viauc.dk>
2022-05-16 13:02:36 +0200

    fixing lists

--- app/src/main/java/com/example/todo4u/Adapters/MemberAdapter.java
+++ app/src/main/java/com/example/todo4u/Adapters/MemberAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,13 +12,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.todo4u.Models.Board;
 import com.example.todo4u.Models.Member;
 import com.example.todo4u.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MemberAdapter extends RecyclerView.Adapter<MemberAdapter.ViewHolder>{
 
     private OnClickListener listener;
     private ArrayList<Member> members;
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://todo4u-16517-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
 
     public MemberAdapter(){
         members = new ArrayList<>();
@@ -61,5 +69,31 @@ public class MemberAdapter extends RecyclerView.Adapter<MemberAdapter.ViewHolder
 
     public interface OnClickListener {
         void onClick(Member member);
+    }
+
+    public void getMemberData()
+    {
+        databaseReference.child("member").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                HashMap<String,String> value =(HashMap<String, String>)snapshot.getValue();
+
+                members
+                        = value.values().toArray(new String[0]);
+
+                notifyDataSetChanged();
+
+                for (int i=0; i<members.length; i++)
+                {
+                    System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!"+members[i]);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(context, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
